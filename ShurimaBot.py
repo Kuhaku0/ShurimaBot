a0cea4e3d43ab9b3f3017870de53f0e9dd9e9d25
Guillaume LEPELLETIER <215284@supinfo.com>
2018-03-27 15:45:12 +0200

    Optimisation + New Functions

--- ShurimaBot.py
+++ ShurimaBot.py
@@ -8,32 +8,39 @@ from discord.ext import commands
 Shurima = discord.Client()
 bot_prefix = "s."
 shurima = commands.Bot(command_prefix=bot_prefix)
+summoners_list = dict()
 
 
 @shurima.event
 async def on_ready():
 
     shurima.send_message(shurima.get_channel("427772573313531904"), "use command s.h")
+    await shurima.change_presence(game=discord.Game(name="s.h"))
 
 
 def get_info(summoner_name):
 
     url = 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}'\
         .format(summoner_name, API_KEY)
-    r = requests.get(url)
-    result = r.json()
+    result = requests.get(url).json()
     return result
 
 
 @shurima.command(pass_context=True)
 async def summoner(summoner_name):
-
-    name = summoner_name.message.content[11:]
+    global summoners_list
+    if str(summoner_name.message.content) == "s.summoner":
+        name = summoners_list.get(str(summoner_name.message.author))
+    else:
+        name = summoner_name.message.content[11:]
     info = get_info(name)
     summoner_id = info["id"]
+    summoner_level = info["summonerLevel"]
     url = 'https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/{}?api_key={}'\
         .format(summoner_id, API_KEY)
     result = requests.get(url).json()
+
+    await shurima.say("{} : Level {} :".format(name, summoner_level))
 
     if result:
         queue_type = ""
@@ -45,12 +52,12 @@ async def summoner(summoner_name):
             elif i["queueType"] == 'RANKED_FLEX_TT':
                 queue_type = "Flex 3c3"
 
-            ranking = "{} : {} : Rank : {} {} | League Points : {} | Wins : {} | Losses {}". \
-                format(name, queue_type, i['tier'], i['rank'], i['leaguePoints'], i['wins'], i['losses'])
+            ranking = "{} : Rank : {} {} | League Points : {} | Wins : {} | Losses {}". \
+                format(queue_type, i['tier'], i['rank'], i['leaguePoints'], i['wins'], i['losses'])
 
             await  shurima.say(ranking)
     else:
-        await shurima.say("{} : Unranked".format(name))
+        await shurima.say("Unranked")
 
 
 @shurima.command(pass_context=True)
@@ -84,15 +91,33 @@ async def h(ctx):
         await shurima.say("List of Command : \n"
                           "1. s.h \n"
                           "2. s.config \n"
-                          "3. s.summoner [summoner_name]")
+                          "3. s.summoner [summoner_name] \n"
+                          "4. s.bestplayer")
     else:
         await shurima.say("List of Command : \n"
                           "1. s.h \n"
-                          "2. s.summoner [summoner_name]")
+                          "2. s.summoner [summoner_name] \n"
+                          "3. s.bestplayer")
+
+
+@shurima.command(pass_context=True)
+async def add_summoner(summoner_name):
+
+    summoners = dict()
+    summoners[str(summoner_name.message.author)] = str(summoner_name.message.content[15:])
+    global summoners_list
+    summoners_list.update(summoners)
+
+
+@shurima.command(pass_context=True)
+async def bestplayer():
+    await shurima.say("Drouyg Beard est le plus Mauvais :3")
 
 
 @shurima.command(pass_context=True)
 async def test():
+
     await shurima.say("test function")
+
 
 shurima.run(TOKEN)
