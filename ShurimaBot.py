052725e020d459d7f9b16938119e81b003814fc9
Guillaume LEPELLETIER <215284@supinfo.com>
2018-03-27 17:17:53 +0200

    Modification Layout Bot

--- ShurimaBot.py
+++ ShurimaBot.py
@@ -28,7 +28,9 @@ def get_info(summoner_name):
 
 @shurima.command(pass_context=True)
 async def summoner(summoner_name):
+
     global summoners_list
+
     if str(summoner_name.message.content) == "s.summoner":
         name = summoners_list.get(str(summoner_name.message.author))
     else:
@@ -39,25 +41,24 @@ async def summoner(summoner_name):
     url = 'https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/{}?api_key={}'\
         .format(summoner_id, API_KEY)
     result = requests.get(url).json()
-
-    await shurima.say("{} : Level {} :".format(name, summoner_level))
+    em = discord.Embed(title="{}".format(name), description="Level : {}".format(summoner_level))
 
     if result:
         queue_type = ""
         for i in result:
             if i["queueType"] == 'RANKED_SOLO_5x5':
-                queue_type = "Solo/Duo"
+                em.add_field(name="Solo/Duo", value="{} : Rank : {} {} | League Points : {} | Wins : {} | Losses {}"
+                             .format(queue_type, i['tier'], i['rank'], i['leaguePoints'], i['wins'], i['losses']))
             elif i["queueType"] == 'RANKED_FLEX_SR':
-                queue_type = "Flex"
+                em.add_field(name="Flex 5c5", value="{} : Rank : {} {} | League Points : {} | Wins : {} | Losses {}"
+                             .format(queue_type, i['tier'], i['rank'], i['leaguePoints'], i['wins'], i['losses']))
             elif i["queueType"] == 'RANKED_FLEX_TT':
-                queue_type = "Flex 3c3"
-
-            ranking = "{} : Rank : {} {} | League Points : {} | Wins : {} | Losses {}". \
-                format(queue_type, i['tier'], i['rank'], i['leaguePoints'], i['wins'], i['losses'])
-
-            await  shurima.say(ranking)
+                em.add_field(name="Flex 3c3", value="{} : Rank : {} {} | League Points : {} | Wins : {} | Losses {}"
+                             .format(queue_type, i['tier'], i['rank'], i['leaguePoints'], i['wins'], i['losses']))
     else:
-        await shurima.say("Unranked")
+        em.add_field(name="Unraked", value="None")
+
+    await shurima.say(embed=em)
 
 
 @shurima.command(pass_context=True)
@@ -88,18 +89,20 @@ async def h(ctx):
     author_role = ctx.message.author.roles
 
     if "admin" in [i.name.lower() for i in author_role]:
-        await shurima.say("List of Command : \n"
-                          "1. s.h \n"
-                          "2. s.config \n"
-                          "3. s.summoner [summoner_name] \n"
-                          "4. s.add_summoner [summoner_name] \n"
-                          "5. s.bestplayer")
+        em = discord.Embed(title="Liste Of commands", description="List of Command : \n"
+                                                                  "1. s.h \n"
+                                                                  "2. s.summoner [summoner_name] \n"
+                                                                  "3. s.bestplayer \n"
+                                                                  "4. s.add_summoner [summoner_name]\n"
+                                                                  "5. s.config")
+        await shurima.say(embed=em)
     else:
-        await shurima.say("List of Command : \n"
-                          "1. s.h \n"
-                          "2. s.summoner [summoner_name] \n"
-                          "3. s.bestplayer \n"
-                          "4. s.add_summoner [summoner_name]")
+        em = discord.Embed(title="Liste Of commands", description="List of Command : \n"
+                                                                  "1. s.h \n"
+                                                                  "2. s.summoner [summoner_name] \n"
+                                                                  "3. s.bestplayer \n"
+                                                                  "4. s.add_summoner [summoner_name]")
+        await shurima.say(embed=em)
 
 
 @shurima.command(pass_context=True)
@@ -113,6 +116,7 @@ async def add_summoner(summoner_name):
 
 @shurima.command(pass_context=True)
 async def bestplayer():
+
     await shurima.say("Drouyg Beard est le plus Mauvais :3")
 
 
@@ -120,6 +124,29 @@ async def bestplayer():
 async def test():
 
     await shurima.say("test function")
+    '''embed = discord.Embed(title="title ~~(did you know you can have markdown here too?)~~",
+                          colour=discord.Colour(0xda4294), url="https://discordapp.com",
+                          description="this supports on top of the previously shown subset of markdown.",
+                          timestamp=datetime.datetime.utcfromtimestamp(1522161520))
+
+    embed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.set_author(name="author name", url="https://discordapp.com",
+                     icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.set_footer(text="footer text", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+
+    embed.add_field(name="🤔", value="some of these properties have certain limits...")
+    embed.add_field(name="😱", value="try exceeding some of them!")
+    embed.add_field(name="🙄",
+                    value="an informative error should show up, and this view will remain as-is until all issues are 
+                    fixed")
+    embed.add_field(name="<:thonkang:219069250692841473>", value="these last two", inline=True)
+    embed.add_field(name="<:thonkang:219069250692841473>", value="are inline fields", inline=True)
+
+    await shurima.say(
+        content="this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n
+        }\n\nfoo(1);```",
+        embed=embed)'''
 
 
 shurima.run(TOKEN)
